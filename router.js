4c8f34a299cd3348c05c7a8611a2f75959d42f98
Jimmy Guo <jimmyzidanguo@hotmail.com>
2019-03-11 14:59:36 -0400

    first commit

---
+++ router.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser')
+// routes
+const Test = require('./routes/test_routes')
+
+
+// bodyParser attempts to parse any request into JSON format
+const json_encoding = bodyParser.json({type:'*/*'})
+// bodyParser attempts to parse any request into GraphQL format
+// const graphql_encoding = bodyParser.text({ type: 'application/graphql' })
+
+module.exports = function(app){
+
+	// routes
+	app.get('/test', json_encoding, Test.test)
+
+}
